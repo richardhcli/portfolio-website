07beb665793ba4e280eae877521f753256f2b4c8
richardhcli <71855022+richardhcli@users.noreply.github.com>
2026-05-25 14:04:17 -0400

    stop purgecrss from pseudo-class selectors

--- purgecss.config.js
+++ purgecss.config.js
@@ -5,9 +5,9 @@ module.exports = {
   skippedContentGlobs: ["_site/assets/**/*.html"],
 
   // JS-added and compound pseudo-class selectors (e.g. :last-child, :has) must survive purging
-  // safelist: {
-  //   standard: [/^project-timeline-/],
-  //   deep: [/^project-timeline-/],
-  //   greedy: [/^project-timeline-/],
-  // },
+  safelist: {
+    standard: [/^project-timeline-/],
+    deep: [/^project-timeline-/],
+    greedy: [/^project-timeline-/],
+  },
 };
