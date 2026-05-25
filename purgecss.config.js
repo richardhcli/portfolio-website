53f82583bfd32439fbdd0719cb36a0a53377c44b
richardhcli <71855022+richardhcli@users.noreply.github.com>
2026-05-25 13:51:51 -0400

    fix segment display on projects list

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,11 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+
+  // JS-added and compound pseudo-class selectors (e.g. :last-child, :has) must survive purging
+  // safelist: {
+  //   standard: [/^project-timeline-/],
+  //   deep: [/^project-timeline-/],
+  //   greedy: [/^project-timeline-/],
+  // },
 };
